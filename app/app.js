eda40d7394d74ae81d8963c79f7ada5648476b04
Krasimir <krasimir.st.tsonev@gmail.com>
2025-09-27 12:49:16 +0300

    Wrapping up the logic

--- app/app.js
+++ app/app.js
@@ -1,28 +1,33 @@
-let current = 0;
+let current = INITIAL_CURRENT;
 let writingInt = null;
-const writingSpeed = 50;
+const writingSpeed = 100;
+let isRan = false;
 
 function init() {
-  console.log(`# of bugs: ${BUGS.length}`)
-  current = 0;
-  showTheBugCase();
+  console.log(`# of slides: ${SLIDES.length}`);
+  current = INITIAL_CURRENT;
+  showSlide();
 }
 function next() {
   console.log('next');
-  if(current < BUGS.length - 1) {
+  if(current < SLIDES.length - 1) {
     current++;
   }
-  showTheBugCase();
+  showSlide();
 }
 function previous() {
   console.log("previous");
   if(current > 0) {
     current--;
   }
-  showTheBugCase();
+  showSlide();
 }
 function onKeydown(event) {
   if (event.key === "Enter") {
+    if (isRan) {
+      next();
+      return;
+    }
     run();
   } else if (event.key === "ArrowRight") {
     next();
@@ -32,21 +37,48 @@ function onKeydown(event) {
     init();
   }
 }
-function showTheBugCase() {
-  console.log(`showTheBugCase ${current}`);
-  const bug = BUGS[current];
+function write(container, text, speed = writingSpeed, cssClass = "cli") {
+  const CLI = document.createElement("div");
+  let charIndex = 0;
+  container.appendChild(CLI);
+  CLI.classList.add(cssClass);
+
+  (function go() {
+    if (charIndex < text.length) {
+      CLI.textContent += text[charIndex];
+      charIndex++;
+      writingInt = setTimeout(go, speed);
+    } else {
+      clearTimeout(writingInt);
+      writingInt = null;
+    }
+  })();
+}
+function showSlide() {
+  console.log(`showSlide ${current}`);
+  isRan = false;
+  clearTimeout(writingInt);
+  document.body.classList.remove('ran');
 
   const root = $("#app");
   root.innerHTML = '';
 
   setTimeout(() => {
-    const CLI = document.createElement("div");
-    root.adppendChild(CLI);
+    write(root, SLIDES[current][0]);
   }, 0);
 }
 function run() {
   console.log(`run ${current}`);
   const root = $("#app");
+  const code = SLIDES[current][0];
+  try {
+    const result = (new Function(`return ${code}`))();
+    write(root, `${result}`, 0, 'result');
+    document.body.classList.add('ran');
+  } catch(err) {
+    console.error(err);
+  }
+  isRan = true;
 }
 
 window.addEventListener('load', init);
